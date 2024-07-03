c06b22f9d174d68f5b1b6bd8229723101d3bc95b
Carlesso041 <111372150+Carlesso041@users.noreply.github.com>
2024-07-03 08:23:15 -0300

    "first commit"

---
+++ back end (JS)/prova.py
@@ -0,0 +1,8 @@
+print("mes de nascimento")
+
+print("janeiro","fevereiro","março,""abril","maio","junho","julho","agosto")
+
+meses={"janeiro","fevereiro","março,""abril"}
+for mes in meses:
+  if mes =="agosto":
+      print('nasci em'  , mes)
